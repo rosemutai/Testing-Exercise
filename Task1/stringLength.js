8d81ca52e89ab5fa52eb151fe5802720bdbcca72
rosemutai <chepngetichrose2030@gmail.com>
2023-04-04 09:39:40 +0300

    Throw error if length of string is less than 1 and greater than 10

--- Task1/stringLength.js
+++ Task1/stringLength.js
@@ -1,7 +1,12 @@
 const stringLength = (string) => {
-  return string.length;
+  if (string.length >= 1 && string.length <= 10) {
+    return string.length;
+  } else {
+    throw new Error('String must be at least1 character long and not longer than 10');
+  }
+  
 };
 
-stringLength('Hello');
+stringLength('Hey');
 
 module.exports = stringLength;
